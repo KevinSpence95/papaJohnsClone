62eca4fe0f4c922bcdbeb973eb19d1145bd0c0d4
Kevin Spence <KevinSpence95@gmail.com>
2022-12-01 17:57:35 -0500

    using config file, and now we can use npm run build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash'
+import myName from './myname';
 
 function nonReactComponent() {
     const element = document.createElement('div')
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.innerHTML = _.join([`Hello ${myName('Kevin')}`, 'this is webpack!'], ' ')
 
     return element;
 }
