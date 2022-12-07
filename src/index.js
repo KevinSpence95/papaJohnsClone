738c02b85698c9499bc0141016b4b15949edf965
Kevin Spence <KevinSpence95@gmail.com>
2022-12-07 15:18:04 -0500

    Load CSS and Image Assets, source maps indicate error loaction properly

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+import './style.css'
+import Mountains from './Mountains.jpg'
+
 import _ from 'lodash'
 import myName from './myname';
+import printMe from './print';
+
+
 
 function nonReactComponent() {
     const element = document.createElement('div')
 
-    element.innerHTML = _.join([`Hello ${myName('Kevin')}`, 'this is webpack!'], ' ')
+    element.innerHTML = _.join([`${myName('Kevin')}`, 'this is webpack!'], ' ')
+
+    element.classList.add('hello')
+
+    const childPic = new Image();
+    childPic.src = Mountains;
+    element.appendChild(childPic)
+
+    const childH1 = document.createElement('h1')
+    childH1.textContent = 'Where is the font?'
+    element.appendChild(childH1)
+
+    printMe()
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    // entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/i,
+                type: 'asset/resource',
+            },         
+        ],
+    }
 };
