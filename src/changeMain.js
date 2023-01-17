2de594f83928bfdf005055b067e94ce28b8a0cf8
Kevin Spence <KevinSpence95@gmail.com>
2023-01-16 22:24:05 -0500

    page navigation sorter, still need to add functionality to the footer link dropdowns on narrow screens

--- src/changeMain.js
+++
@@ -1,9 +0,0 @@
-import home from "./home";
-import menu from "./menu";
-import specials from "./specials";
-import papaRewards from "./papaRewards";
-
-
-export default function changeMain(mainContent) {
-  
-}
--- src/index.js
+++ src/index.js
@@ -1,33 +1,67 @@
-import './style.css'
+import "./style.css";
+import home from "./home";
+import menu from "./menu";
+import specials from "./specials";
+import papaRewards from "./papaRewards";
 
 // import Mountains from "./Mountains.jpg";
 
-function changePapaJohnsMainContent() {
-//clear mainContent section
-document.querySelector(".mainContent").innerHTML = "";
+let main = document.body.querySelector(".mainContent");
 
-//return appropriate
-let content;
-
-switch (mainContent) {
-  case mainContent === "home":
-    content = home();
-    break;
-  case mainContent === "menu":
-    content = menu();
-    break;
-  case mainContent === "specials":
-    content = specials();
-    break;
-  case mainContent === "papaRewards":
-    content = papaRewards();
-    break;
-  default:
-    content = home();
-}
-return content;
+function changeMain(page) {
+  main.removeChild(main.firstChild);
+  let contentElement;
+  if (page == "home") {
+    contentElement = home();
+  } else if (page == "menu") {
+    contentElement = menu();
+  } else if (page == "specials") {
+    contentElement = specials();
+  } else if (page == "papa rewards") {
+    contentElement = papaRewards();
+  }
+  main.appendChild(contentElement);
 }
 
-const main = document.querySelector('.mainContent')
+changeMain("home");
 
-main.appendChild(papaJohns());
+let logoTarget = document.querySelector(".logo img");
+logoTarget.addEventListener("click", () => {
+  changeMain("home");
+});
+
+let syoTarget = document.querySelector(".startYourOrder a");
+syoTarget.addEventListener("click", () => {
+  changeMain("specials");
+});
+
+let signUpTarget = document.querySelector(".signUp a");
+signUpTarget.addEventListener("click", () => {
+  changeMain("home");
+});
+
+let menuTarget = document.querySelector(".menu a");
+menuTarget.addEventListener("click", () => {
+  changeMain("menu");
+});
+
+let specialsTarget = document.querySelector(".specials a");
+specialsTarget.addEventListener("click", () => {
+  changeMain("specials");
+});
+
+let papaRewardsTarget = document.querySelector(".papaRewards a");
+papaRewardsTarget.addEventListener("click", () => {
+  changeMain("papa rewards");
+});
+
+let shoppingCartTarget = document.querySelector(".cart");
+shoppingCartTarget.addEventListener("click", () => {
+  changeMain("specials");
+});
+
+
+
+// const logoTarget = document.querySelector(".logo img");
+// logoTarget.onClick = () => {changeMain('specials')}
+// logoTarget.addEventListener("click", changeMain("specials"));
